5e5ddd379d3241103c13d2d5c8a7801a827bf0b3
shivaAg04 <81668752+shivaAg04@users.noreply.github.com>
2023-09-22 00:21:21 +0530

    modified border

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Flutter Demo', theme: ThemeData(), home: const HomePage());
+        title: 'Flutter Demo',
+        theme: ThemeData(primaryColor: Colors.green),
+        home: const HomePage());
   }
 }
